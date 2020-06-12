05d368bbc7b3daf25389627909e214aef6298f11
bingis khan <robert.noparlik@gmail.com>
2020-06-13 00:30:31 +0200

    Trash deleted

--- main/java/mlsim/operators/Crossover.java
+++
@@ -1,7 +0,0 @@
-package mlsim.operators;
-
-import mlsim.util.Tuple;
-
-public interface Crossover<T extends Crossover<T>> {
-	Tuple<T, T> crossover(T other);
-}
--- main/java/mlsim/operators/Mutable.java
+++
@@ -1,7 +0,0 @@
-package mlsim.operators;
-/**
- *  Interface is responsible for mutate object
- */
-public interface Mutable<T extends Mutable<T>> {
-	T mutate();
-}
