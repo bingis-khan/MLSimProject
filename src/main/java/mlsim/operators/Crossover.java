0726f206349e12dcb24f2713087744b4ba8c891c
bingis-khan <52740618+bingis-khan@users.noreply.github.com>
2020-06-13 01:42:05 +0200

    rip bong DAB *LIT*
    QAQ

--- src/main/java/mlsim/operators/Crossover.java
+++ src/main/java/mlsim/operators/Crossover.java
@@ -2,6 +2,10 @@ package mlsim.operators;
 
 import mlsim.util.Tuple;
 
+/**
+ * Crossover is an interface that represents the crossover operation
+ *  and the ability to perform it.
+ */
 public interface Crossover<T extends Crossover<T>> {
 	Tuple<T, T> crossover(T other);
 }
